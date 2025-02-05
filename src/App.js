4cbca2bf591f9ee3d59368504171d11642271b29
bprasantpatnaik2002 <bprasant.patnaik@aiordinate.com>
2025-02-05 21:52:17 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -242,6 +242,7 @@ app.post("/blogs", authenticateUser, async (req, res) => {
     try {
         const newData = new Blog(req.body);
         await newData.save();
+        console.log("New Data Saved");
     } catch (error) {
         res.status(500).json({ error: "Server error" });
         
@@ -252,6 +253,7 @@ app.delete("/blogs/:id", authenticateUser, async (req, res) => {
     
     try {
         await Blog.findByIdAndDelete(req.params.id);
+        console.log("New Data deleted");
     } catch (error) {
         res.status(500).json({ error: "Server error" });
         
