5e655d385e1b484a53d2a1af22707b23f8c379e9
Unleashed01 <b421014@iiit-bh.ac.in>
2025-03-07 12:26:52 +0530

    Refactor CouponSchema: change 'validUntil' field type from Date to String

--- src/models/CouponSchema.js
+++ src/models/CouponSchema.js
@@ -5,7 +5,7 @@ const CouponSchema = new mongoose.Schema({
     code: { type: String, required: true, unique: true, trim: true },
     discountType: { type: String, enum: ["percentage", "fixed"], required: true },
     discountValue: { type: Number, required: true },
-    validUntil: { type: Date, required: true },
+    validUntil: { type: String, required: true },
     isActive: { type: Boolean, default: true },
 },{timestamps:true});
 
